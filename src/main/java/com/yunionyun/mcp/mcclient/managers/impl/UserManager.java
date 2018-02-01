d0fe24170ac22cd8d47d38affa58aed6307de143
Qiu Jian <qiujian@yunionyun.com>
2018-02-01 17:46:26 +0800

    增加 user.getProjects() 和 user.getGroups() 接口

--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/UserManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/UserManager.java
@@ -1,6 +1,8 @@
 package com.yunionyun.mcp.mcclient.managers.impl;
 
+import com.yunionyun.mcp.mcclient.Session;
 import com.yunionyun.mcp.mcclient.managers.KeystoneManager;
+import com.yunionyun.mcp.mcclient.managers.ListResult;
 
 public class UserManager extends KeystoneManager {
 	public UserManager() {
@@ -9,4 +11,25 @@ public class UserManager extends KeystoneManager {
                 new String[]{"ID", "Name", "Domain_Id",
                         "Enabled", "Email", "Mobile"});
 	}
+
+	public ListResult getProjects(Session s, String uId) {
+		String url = "/users/" + uId + "/projects";
+		try {
+			return this._list(s, url, "projects");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public ListResult getGroups(Session s, String uId) {
+		String url = "/users/" + uId + "/groups";
+		try {
+			return this._list(s, url, "projects");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 }
