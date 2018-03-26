577b7502e32126a50da601381113f50620032129
ningyu <ningyu@yunionyun.com>
2018-03-26 17:19:25 +0800

    fix: 去掉HostManager中admin列出的sn

--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/HostManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/HostManager.java
@@ -8,6 +8,6 @@ public class HostManager extends ComputeManager
     {
         super("host", "hosts", 
                 new String[] {"ID", "Name", "Access_mac", "Access_ip", "Manager_URI", "Status", "enabled", "host_status", "Guests", "Running_guests", "storage", "storage_used", "storage_virtual", "disk_used", "mem_size", "mem_used", "mem_free", "mem_commit", "cpu_count", "cpu_used", "cpu_commit", "cpu_commit_rate", "mem_commit_rate", "cpu_commit_bound", "mem_commit_bound", "sn", "storage_type", "host_type", "version", "schedtags", "storage_size"},
-                new String[] { "sn" });
+                new String[] {});
     }
 }
