d04cf5bd1bb0f0c07909b30f465e049ffaacbb52
ningyu <ningyu@yunionyun.com>
2018-03-26 14:37:35 +0800

    fix: 补充mod_hosts中的字段到Hostmanager中

--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/HostManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/HostManager.java
@@ -7,7 +7,7 @@ public class HostManager extends ComputeManager
     public HostManager()
     {
         super("host", "hosts", 
-                new String[] { "ID", "Name", "Access_mac", "Access_ip", "Manager_URI", "Status", "enabled", "host_status", "mem_size", "cpu_count", "storage_type", "host_type", "version" }, 
+                new String[] {"ID", "Name", "Access_mac", "Access_ip", "Manager_URI", "Status", "enabled", "host_status", "Guests", "Running_guests", "storage", "storage_used", "storage_virtual", "disk_used", "mem_size", "mem_used", "mem_free", "mem_commit", "cpu_count", "cpu_used", "cpu_commit", "cpu_commit_rate", "mem_commit_rate", "cpu_commit_bound", "mem_commit_bound", "sn", "storage_type", "host_type", "version", "schedtags", "storage_size"},
                 new String[] { "sn" });
     }
 }
