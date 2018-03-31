1d8542575d026ee86e01819aeea7862e0a50fa8f
ningyu <ningyu@yunionyun.com>
2018-03-31 11:09:54 +0800

    feat: 添加列出全部监控指标的API

--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/MonitorMetricManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/MonitorMetricManager.java
@@ -6,32 +6,51 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.yunionyun.mcp.mcclient.Session;
 import com.yunionyun.mcp.mcclient.managers.BaseMonitorManager;
+import com.yunionyun.mcp.mcclient.managers.ListResult;
 import com.yunionyun.mcp.mcclient.managers.ManagerContext;
 
-public class MonitorMetricManager extends BaseMonitorManager {
-
-	public MonitorMetricManager() {
-		super("metric", "metrics",
-		        new String[] {"id", "name", "description", "unit", "common_unit", "status", "create_by", "update_by", "delete_by", "gmt_create", "gmt_modified", "gmt_delete", "is_deleted", "project_id", "remark"},
-		        new String[] {});
-	}
-	
-	public void batchSubmit(Session s, Set<String> sets) throws Exception 
-	{
-	    JSONArray metrics = new JSONArray();
-	    for (String name : sets)
+public class MonitorMetricManager extends BaseMonitorManager
+{
+    
+    public MonitorMetricManager()
+    {
+        super("metric", "metrics", new String[] { "id", "name", "description", "unit", "common_unit", "status", "create_by", "update_by", "delete_by", "gmt_create", "gmt_modified", "gmt_delete",
+                "is_deleted", "project_id", "remark" }, new String[] {});
+    }
+    
+    public void batchSubmit(Session s, Set<String> sets) throws Exception
+    {
+        JSONArray metrics = new JSONArray();
+        for (String name : sets)
         {
-	        JSONObject node = new JSONObject();
-	        node.put("name", name);
-	        metrics.add(node);
+            JSONObject node = new JSONObject();
+            node.put("name", name);
+            metrics.add(node);
         }
-	    
-	    JSONObject params = new JSONObject();
-		params.put("metrics", metrics);
-		
-		StringBuilder url = this.getContextPath(new ManagerContext[] {});
+        
+        JSONObject params = new JSONObject();
+        params.put("metrics", metrics);
+        
+        StringBuilder url = this.getContextPath(new ManagerContext[] {});
         url.append(this.urlKey());
         
         this._post(s, url.toString(), params, null);
-	}
+    }
+    
+    public ListResult listAll(Session s) throws Exception
+    {
+        ListResult tmpList = this.List(s, null);
+        int dataSize = tmpList.getDataSize();
+        
+        if (tmpList.getTotal() <= dataSize)
+        {
+            return tmpList;
+        }
+        
+        JSONObject query = new JSONObject();
+        query.put("offset", 0);
+        query.put("limit", tmpList.getTotal());
+        
+        return this.List(s, query);
+    }
 }
--- src/test/java/com/yunionyun/mcp/mcclient/MetricTest.java
+++ src/test/java/com/yunionyun/mcp/mcclient/MetricTest.java
@@ -6,15 +6,17 @@ import java.util.Set;
 
 import org.slf4j.Logger;
 
+import com.alibaba.fastjson.JSONObject;
 import com.yunionyun.mcp.mcclient.keystone.TokenCredential;
+import com.yunionyun.mcp.mcclient.managers.ListResult;
 import com.yunionyun.mcp.mcclient.managers.impl.MonitorMetricManager;
 import com.yunionyun.mcp.mcclient.utils.LoggerUtils;
 
 import junit.framework.TestCase;
 
-public class BatchAddMetricTest extends TestCase
+public class MetricTest extends TestCase
 {
-    private static Logger logger = LoggerUtils.createLoggerFor(BatchAddMetricTest.class.getName());
+    private static Logger logger = LoggerUtils.createLoggerFor(MetricTest.class.getName());
     
     
     public void testApp()
@@ -29,7 +31,17 @@ public class BatchAddMetricTest extends TestCase
             MonitorMetricManager manager = new MonitorMetricManager();
             
             Set<String> sets = new HashSet<String>(Arrays.asList("test_metric01", "test_metric02"));
+            
+            // Test: Batch add metrics
             //manager.batchSubmit(s, sets);
+            
+            // Test: List all metrics
+            ListResult listAll = manager.listAll(s);
+            for (int i = 0; i < listAll.getDataSize(); i++)
+            {
+                JSONObject object = listAll.getDataAt(i);
+                logger.debug(object.toJSONString());
+            }
         }
         catch (Exception e)
         {
