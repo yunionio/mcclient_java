31d3d335fd848ed5b7b8f627e9a8850d6e1542e5
Qiu Jian <qiujian@yunionyun.com>
2018-04-13 16:50:16 +0800

    Java httpUrlConnection 支持Patch方法

---
+++ src/test/java/com/yunionyun/mcp/mcclient/PatchTest.java
@@ -0,0 +1,31 @@
+package com.yunionyun.mcp.mcclient;
+
+import com.alibaba.fastjson.JSONObject;
+import com.yunionyun.mcp.mcclient.keystone.TokenCredential;
+import com.yunionyun.mcp.mcclient.managers.ListResult;
+import com.yunionyun.mcp.mcclient.managers.impl.ProjectManager;
+
+import junit.framework.TestCase;
+
+public class PatchTest extends TestCase {
+	public void testApp()
+    {
+        Client cli = new Client("http://10.168.26.235:5000/v3", 1000, true, true);
+        try {
+        		TokenCredential token = cli.Authenticate("sysadmin", "sysadmin", "Default", "system");
+        		Session s = cli.newSession("TestLocal", null, null, token);
+        		ProjectManager projman = new ProjectManager();
+        		JSONObject results = projman.GetByName(s, "system", null);
+        		String id = results.getString("id");
+        		JSONObject params = new JSONObject();
+        		params.put("URL", "http://wwww.sina.com.cn");
+        		projman.Patch(s, id, params);
+        		results = projman.GetByName(s, "system", null);
+        		for (String key: results.keySet()) {
+        			System.out.print(key + ": " + results.getString(key));
+        		}
+        }catch(Exception e) {
+        		e.printStackTrace();
+        }
+    }
+}
