baf38bade123fe74c875355ef208182ae5bdecbd
Zheng Jiajie <zhengjiajie@yunionyun.com>
2018-04-21 15:48:18 +0800

    0421--提交assgin和unassign的方法

--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/UserManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/UserManager.java
@@ -1,5 +1,6 @@
 package com.yunionyun.mcp.mcclient.managers.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.yunionyun.mcp.mcclient.Session;
 import com.yunionyun.mcp.mcclient.managers.KeystoneManager;
 import com.yunionyun.mcp.mcclient.managers.ListResult;
@@ -31,5 +32,45 @@ public class UserManager extends KeystoneManager {
 		}
 		return null;
 	}
-
+	
+	/**
+	 * /v3/domains/{domain_id}/users/{user_id}/roles/{role_id}
+	 * Assign role to user on domain
+	 * @param s
+	 * @param domainId
+	 * @param userId
+	 * @param roleId
+	 * @return
+	 */
+	public JSONObject assignRoleToUserOnDomain(Session s, String domainId, String userId, String roleId) {
+		String url = "/domains/"+domainId+"/users/"+userId+"/roles/"+roleId;
+		try {
+			return this._put(s, url, null, this.keyword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 *  /v3/domains/{domain_id}/users/{user_id}/roles/{role_id}
+	 *  Unassigns role from user on domain
+	 * @param s
+	 * @param domainId
+	 * @param userId
+	 * @param roleId
+	 * @return
+	 */
+	public JSONObject unassignRoleToUserOnDomain(Session s, String domainId, String userId, String roleId) {
+		String url = "/domains/"+domainId+"/users/"+userId+"/roles/"+roleId;
+		try {
+			return this._delete(s, url, null, this.keyword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/* 
+	*/
 }
