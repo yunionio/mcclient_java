bc463624fbf08b9188b930e1935f7bdb444627f6
ningyu <ningyu@yunionyun.com>
2018-08-13 19:18:05 +0800

    feat: 添加cloud-provider-*支持

---
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/CloudProviderManager.java
@@ -0,0 +1,11 @@
+package com.yunionyun.mcp.mcclient.managers.impl;
+
+import com.yunionyun.mcp.mcclient.managers.ComputeManager;
+
+public class CloudProviderManager extends ComputeManager
+{
+    public CloudProviderManager()
+    {
+        super("cloudprovider", "cloudproviders", new String[] { "id", "access_url", "account", "enabled", "is_emulated", "last_sync", "name", "provider", "status" }, new String[] {});
+    }
+}
