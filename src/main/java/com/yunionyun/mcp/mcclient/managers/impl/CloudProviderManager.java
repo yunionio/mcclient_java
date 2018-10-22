090cc6027db2f39c09f0c2634be29a347cc9271c
Zheng Jiajie <zhengjiajie@yunionyun.com>
2018-10-22 09:14:48 +0800

    修改CloudProvider的关键字为cloudaccount

--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/CloudProviderManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/CloudProviderManager.java
@@ -6,6 +6,7 @@ public class CloudProviderManager extends ComputeManager
 {
     public CloudProviderManager()
     {
-        super("cloudprovider", "cloudproviders", new String[] { "id", "access_url", "account", "enabled", "is_emulated", "last_sync", "name", "provider", "status" }, new String[] {});
+    	super("cloudaccount", "cloudaccounts", new String[] { "id", "access_url", "account", "enabled", "is_emulated", "last_sync", "name", "provider", "status" }, new String[] {});
+        //super("cloudprovider", "cloudproviders", new String[] { "id", "access_url", "account", "enabled", "is_emulated", "last_sync", "name", "provider", "status" }, new String[] {});
     }
 }
