18492905b6f049fb5b3a83644eb991641e38327b
Zheng Jiajie <zhengjiajie@yunionyun.com>
2018-10-22 11:57:06 +0800

    增加cloudaccount_id字段

--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/CloudProviderManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/CloudProviderManager.java
@@ -6,6 +6,6 @@ public class CloudProviderManager extends ComputeManager
 {
     public CloudProviderManager()
     {
-    	super("cloudprovider", "cloudproviders", new String[] { "id", "access_url", "account", "enabled", "is_emulated", "last_sync", "name", "provider", "status", "tenant_id" }, new String[] {});
+    	super("cloudprovider", "cloudproviders", new String[] { "id", "access_url", "account", "enabled", "is_emulated", "last_sync", "name", "provider", "status", "cloudaccount_id", "tenant_id" }, new String[] {});
     }
 }
