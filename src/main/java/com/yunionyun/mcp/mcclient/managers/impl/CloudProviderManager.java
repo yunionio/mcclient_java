0bb48ae739a52b9d1a94798a8736bad40d0c9e2e
Zheng Jiajie <zhengjiajie@yunionyun.com>
2018-10-22 11:51:53 +0800

    提交CloudProvider和CloudAccount的代码

--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/CloudProviderManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/CloudProviderManager.java
@@ -6,7 +6,6 @@ public class CloudProviderManager extends ComputeManager
 {
     public CloudProviderManager()
     {
-    	super("cloudaccount", "cloudaccounts", new String[] { "id", "access_url", "account", "enabled", "is_emulated", "last_sync", "name", "provider", "status" }, new String[] {});
-        //super("cloudprovider", "cloudproviders", new String[] { "id", "access_url", "account", "enabled", "is_emulated", "last_sync", "name", "provider", "status" }, new String[] {});
+    	super("cloudprovider", "cloudproviders", new String[] { "id", "access_url", "account", "enabled", "is_emulated", "last_sync", "name", "provider", "status", "tenant_id" }, new String[] {});
     }
 }
