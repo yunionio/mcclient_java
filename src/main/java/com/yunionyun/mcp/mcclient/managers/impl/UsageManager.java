1263546f2dc7b329e7bcfd31f941640fab99c03b
ningyu <ningyu@yunionyun.com>
2018-12-05 16:45:59 +0800

    feat: 根据host_type获得usage使用量统计

--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/UsageManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/UsageManager.java
@@ -1,5 +1,8 @@
 package com.yunionyun.mcp.mcclient.managers.impl;
 
+import com.alibaba.fastjson.JSONObject;
+import com.yunionyun.mcp.mcclient.Session;
+import com.yunionyun.mcp.mcclient.Utils;
 import com.yunionyun.mcp.mcclient.managers.ComputeManager;
 
 public class UsageManager extends ComputeManager
@@ -7,5 +10,25 @@ public class UsageManager extends ComputeManager
     public UsageManager()
     {
     	super("usage", "usages", new String[] { "Field", "Value" }, new String[] {});
+    }
+    
+    
+    public JSONObject Get(Session s, JSONObject query) throws Exception
+    {
+        StringBuilder url = new StringBuilder();
+        url.append("/");
+        url.append(this.keywordPlural);
+        
+        if (query != null)
+        {
+            String queryStr = Utils.JSONObject2QueryString(query);
+            if (queryStr.length() > 0)
+            {
+                url.append("?");
+                url.append(queryStr);
+            }
+        }
+        
+        return this._get(s, url.toString(), this.keyword);
     }
 }
