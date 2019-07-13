621982234e838e426f689f4352b8d1921a24a465
mynameisny <mynameisny@qq.com>
2019-07-13 11:47:28 +0800

    refactor: 代码格式化，统一使用空格进行缩进

--- src/main/java/com/yunionyun/mcp/mcclient/managers/impl/ServerManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/impl/ServerManager.java
@@ -8,23 +8,18 @@ import com.yunionyun.mcp.mcclient.Session;
 import com.yunionyun.mcp.mcclient.common.McClientJavaBizException;
 import com.yunionyun.mcp.mcclient.managers.ComputeManager;
 
-public class ServerManager extends ComputeManager {
-	public ServerManager() {
-		super("server", "servers",
-               new String[] {"ID", "Name", "Billing_type",
-            "IPs", "Disk", "Status",
-            "vcpu_count", "vmem_size",
-            "ext_bw", "Zone_name",
-            "Secgroup", "Secgrp_id",
-            "vrouter", "vrouter_id",
-            "Created_at", "Group_name",
-            "Group_id", "Hypervisor"},
-             new String[] {"Host", "Tenant", "is_system"});
-	}
-	
-	public JSONObject changeConfig(Session session, String id, JSONObject body) throws McClientJavaBizException, IOException, JSONClientException
-	{
-	    StringBuilder url = new StringBuilder();
+public class ServerManager extends ComputeManager
+{
+    public ServerManager()
+    {
+        super("server", "servers", new String[] { "ID", "Name", "Billing_type", "IPs", "Disk", "Status", "vcpu_count", "vmem_size", "ext_bw", "Zone_name", "Secgroup", "Secgrp_id", "vrouter",
+                "vrouter_id", "Created_at", "Group_name", "Group_id", "Hypervisor" }, new String[] { "Host", "Tenant", "is_system" });
+    }
+    
+    
+    public JSONObject changeConfig(Session session, String id, JSONObject body) throws McClientJavaBizException, IOException, JSONClientException
+    {
+        StringBuilder url = new StringBuilder();
         url.append("/");
         url.append(this.keywordPlural);
         url.append("/");
@@ -32,5 +27,5 @@ public class ServerManager extends ComputeManager {
         url.append("/change-config");
         
         return this._post(session, url.toString(), body, this.keyword);
-	}
- }
+    }
+}
