f0af1be54069e7b52be26bdc8dc39c6162039d93
zxc <1422928955@qq.com>
2019-09-26 13:11:55 +0800

    更改ComputeManager和QutaManager中构造方法，使在使用时可以动态传参确定internalUrl和publicUrl

--- src/main/java/com/yunionyun/mcp/mcclient/managers/ComputeManager.java
+++ src/main/java/com/yunionyun/mcp/mcclient/managers/ComputeManager.java
@@ -7,4 +7,8 @@ public class ComputeManager extends ResourceManager {
 	public ComputeManager(String keyword, String keywordPlural, String[] columns, String[] adminColumns) {
 		super("compute", EndpointType.InternalURL, null, columns, adminColumns, keyword, keywordPlural, null);
 	}
+
+	public ComputeManager(String keyword, String keywordPlural, EndpointType endpointType, String[] columns, String[] adminColumns) {
+		super("compute", endpointType, null, columns, adminColumns, keyword, keywordPlural, null);
+	}
 }
